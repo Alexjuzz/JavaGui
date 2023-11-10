6c0628c0d878410d7538913b8f7ca9b67a244bab
Alexjuzz <juzzle@mail.ru>
2023-11-10 20:32:01 +0300

    1.3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,11 @@ package org.example;
 import org.example.Client.Client;
 import org.example.server.Server;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
         Server server = new Server();
         new Client(server);
         new Client(server);
+
     }
 }
