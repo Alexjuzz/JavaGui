df595687d3318507743563383c7d66037eb35e94
Alex <juzzle@mail.ru>
2023-11-28 06:21:19 +0300

    ещё не начал делать 4

---
+++ home4/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+/**
+ * Сделаю дз в среду, не успеваю к сожалению сдавать во время, спасибо за понимание.
+ */
+/
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
